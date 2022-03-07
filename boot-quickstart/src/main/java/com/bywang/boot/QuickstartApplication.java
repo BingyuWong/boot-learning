f4414e722101be939e5678b239500b235fa236f1
BingyuWong <100463509+BingyuWong@users.noreply.github.com>
2022-03-07 15:19:51 +0800

    Add files via upload

---
+++ boot-quickstart/src/main/java/com/bywang/boot/QuickstartApplication.java
@@ -0,0 +1,11 @@
+package com.bywang.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartApplication {
+    public static void main(String[] args){
+        SpringApplication.run(QuickstartApplication.class,args);
+    }
+}
